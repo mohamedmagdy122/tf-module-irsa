024783a6c98e0abc4b5fac292068c91af45d879d
mohamedmagdy122 <your-email@gmail.com>
2026-06-05 05:56:36 +0300

    Fix: proper HCL syntax

--- variables.tf
+++ variables.tf
@@ -1,7 +1,23 @@
-variable "role_name" { type = string }
-variable "oidc_provider_arn" { type = string }
-variable "oidc_provider_url" { type = string }
-variable "namespace" { type = string }
-variable "service_account" { type = string }
-variable "policy_arns" { type = list(string); default = [] }
-variable "inline_policy" { type = string; default = "" }
+variable "role_name" {
+  type = string
+}
+variable "oidc_provider_arn" {
+  type = string
+}
+variable "oidc_provider_url" {
+  type = string
+}
+variable "namespace" {
+  type = string
+}
+variable "service_account" {
+  type = string
+}
+variable "policy_arns" {
+  type    = list(string)
+  default = []
+}
+variable "inline_policy" {
+  type    = string
+  default = ""
+}
